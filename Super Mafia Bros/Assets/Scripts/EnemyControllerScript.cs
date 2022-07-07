c521c90887115e40a3461fa981b450095b1d62c0
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2022-07-07 16:21:37 +0530

    Updated version of the game.

--- Super Mafia Bros/Assets/Scripts/EnemyControllerScript.cs
+++ Super Mafia Bros/Assets/Scripts/EnemyControllerScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyControllerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public int enemySpeed;
+    public int xMoveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * enemySpeed;
+        if(hit.distance < 1.85f)
+		{
+            Flip();
+		}
     }
+
+    void Flip()
+	{
+        if(xMoveDirection > 0f)
+		{
+            xMoveDirection = -1;
+		}
+		else
+		{
+            xMoveDirection = 1;
+		}
+	}
+
 }
