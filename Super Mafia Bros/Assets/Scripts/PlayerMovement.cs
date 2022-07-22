a93544f228b9b549572398f803b3a06bd3d220f2
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2022-07-21 18:10:48 -0700

    Updated Version of the game 8.0

--- Super Mafia Bros/Assets/Scripts/PlayerMovement.cs
+++ Super Mafia Bros/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public int playerSpeed = 10;
-    private bool facingRight = false;
     public int playerJumpSpeed = 1250;
     private float moveX;
     public bool isGrounded;
+    public float distanceToBottomOfPlayer = 0.9f;
 
     // Start is called before the first frame update
     //void Start()
@@ -31,14 +31,24 @@ public class PlayerMovement : MonoBehaviour
 		{
             Jump();
 		}
-        if(moveX < 0.0f && facingRight == false)
-		{
-            FlipPlayer();
-		}
-        else if(moveX > 0.0f && facingRight == true)
-		{
-            FlipPlayer();
+
+        if(moveX != 0)
+        {
+            GetComponent<Animator>().SetBool("IsRunning", true);
         }
+        else
+        {
+            GetComponent<Animator>().SetBool("IsRunning", false);
+        }
+        if (moveX < 0.0f )
+		{
+            GetComponent<SpriteRenderer>().flipX = true;
+		}
+        else if(moveX > 0.0f )
+		{
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 	}
 
@@ -48,28 +58,23 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = false;
     }
 
-    void FlipPlayer()
-	{
-        facingRight = !facingRight;
-        Vector2 localScale = gameObject.transform.localScale;
-        localScale.x *= -1;
-        transform.localScale = localScale;
-	}
-
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-        
-	}
+    //void FlipPlayer()
+	//{
+      //  facingRight = !facingRight;
+        //Vector2 localScale = gameObject.transform.localScale;
+        //localScale.x *= -1;
+        //transform.localScale = localScale;
+	//}
 
     void PlayerRaycast()
 	{
         RaycastHit2D rayUp = Physics2D.Raycast(transform.position, Vector2.up);
-        if (rayUp != null && rayUp.collider != null && rayUp.distance < 0.9f && rayUp.collider.name == "Broken_Box")
+        if (rayUp != null && rayUp.collider != null && rayUp.distance < distanceToBottomOfPlayer && rayUp.collider.name == "Broken_Box")
 		{
             Destroy(rayUp.collider.gameObject);
 		}
         RaycastHit2D rayDown = Physics2D.Raycast(transform.position, Vector2.down);
-        if(rayDown != null && rayDown.collider != null && rayDown.distance < 0.9f && rayDown.collider.tag == "enemy")
+        if(rayDown != null && rayDown.collider != null && rayDown.distance < distanceToBottomOfPlayer && rayDown.collider.tag == "enemy")
 		{
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000);
             rayDown.collider.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 200);
@@ -78,7 +83,7 @@ public class PlayerMovement : MonoBehaviour
             rayDown.collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             rayDown.collider.gameObject.GetComponent<EnemyControllerScript>().enabled = false;
         }
-        if (rayDown != null && rayDown.collider != null && rayDown.distance < 0.9f && rayDown.collider.tag != "enemy")
+        if (rayDown != null && rayDown.collider != null && rayDown.distance < distanceToBottomOfPlayer && rayDown.collider.tag != "enemy")
         {
             isGrounded = true;
         }
